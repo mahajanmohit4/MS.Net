dfe43e785483326f672252a62990435441a0159d
mahajanmohit4 <mahajanmohit4@gmail.com>
2020-12-08 22:11:52 +0530

    ..

---
+++ Day8/AnonMethodsAndLembdas/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonMethodsAndLembdas1
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Action o1 = Display;
+            o1();
+
+            Action<string> o2 = Display;
+            o2("Mohit");
+
+            Action<string,int> o3 = Display;
+            o3("Mohit",11);
+            Console.ReadLine();
+        }
+        static void Main2(string[] args)
+        {
+            Func<int, int, int> f1 = Add;
+            Console.WriteLine(f1(11,22));
+
+            Func<string, short, int> f2 = DoSomting;
+            Console.WriteLine(f2("MOhit", 22));
+            Console.ReadLine();
+        }
+        static void Main3(string[] args)
+        {
+            Func<int, int, int> f1 = Add;
+            Console.WriteLine(f1(11, 22));
+
+            Func<string, short, int> f2 = DoSomting;
+            Console.WriteLine(f2("MOhit", 22));
+
+            Func<int, bool> f3 = IsEven;
+            Console.WriteLine(f3(10));
+
+            Predicate<int> f4 = IsEven;
+            Console.WriteLine(f4(3));
+
+            Console.ReadLine();
+        }
+
+        static void Main4(string[] args)
+        {
+            // Action o = delegate { Console.WriteLine("Annonymous mehtod Called"); }
+            int i = 10;
+            Action o = delegate ()
+            {
+                Console.WriteLine("Display");
+                ++i;
+            };
+
+            o();
+            Func<int ,int,int> o2 = delegate(int a, int b)
+        {
+                return a + b;
+            };
+            Console.WriteLine(i);
+            Console.WriteLine(o2(10,20));
+
+            Console.ReadLine();
+        }
+        static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int DoSomting(String a, short b)
+        {
+            return 1;
+        }
+        static void Display()
+        {
+            Console.WriteLine("Display");
+        }
+        static void Display(string s)
+        {
+            Console.WriteLine("Display "+s);
+        }
+        static void Display(string s,int a)
+        {
+            Console.WriteLine("Display " + s+a);
+        }
+    }
+}
+namespace Lambda
+{
+    class Program
+    {
+   
+
+        static void Main(string[] args)
+        {
+            //shorter syntex of the ananomoous
+            //x is the paramter of he function
+            //=> is the lambda operator
+            //x*2 is he return value
+            Func<int, int> o = x => x * 2;
+            Func<int, int, int> o2 = (a, b) => a + b;
+           // Func<int, int, int> o2 = (a, b) => { return a + b; };
+
+            Func<int, int, int, int> o3 = (a, b, c) =>
+               {
+                //multiple line of code
+                return a + b + c;
+               };
+
+            Console.WriteLine(o2(10,20));
+            Console.WriteLine(o(10));
+
+            Console.ReadLine();
+        }
+        static int MakeDouble(int a)
+        {
+            return a * 2;
+        }
+        static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int DoSomting(String a, short b)
+        {
+            return 1;
+        }
+        static void Display()
+        {
+            Console.WriteLine("Display");
+        }
+        static void Display(string s)
+        {
+            Console.WriteLine("Display " + s);
+        }
+        static void Display(string s, int a)
+        {
+            Console.WriteLine("Display " + s + a);
+        }
+    }
+}
---
+++ Day8/LanguageFeatures/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//implicit variable
+namespace LanguageFeatures
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            int i = 11;
+            var i2 = 100; // implicit variable
+            var s = "Mohit";
+            Console.WriteLine(i2.GetType().ToString()) ;
+            Console.ReadLine();
+        }
+        static void Main2(string[] args)
+        {
+            Class1 o = new Class1();
+            o.i = 123;
+            o.j = 456;
+            Class1 o2 = new Class1{ i = 11, j = 22 };
+            Console.ReadLine();
+        }
+    }
+    public class Class1
+    {
+        public int i;
+        public int j;
+
+    }
+}
+
+namespace LanguageFeatures3
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            var myCar = new { Color = "red", Model = "Farrari", Version = "v1" };
+            var myCar1 = new { Color = "red2", Model = "Farrari2", Version = "v12", CurentSpeend =222 };
+
+            Console.WriteLine("{0} {1} {2}",myCar.Color,myCar.Model,myCar.Version);
+
+
+            Console.WriteLine(myCar1.GetType().ToString());
+            Console.WriteLine(myCar.GetType().ToString());
+
+            Console.ReadLine();
+        }
+      
+    }
+
+}
+
+namespace LanguageFeatures4
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            int i = 100;
+            i.Display();
+
+            int j = 456;
+            j.Display();
+
+            // i = i.Reverse() 
+            string s = "aaa";
+            s.Show();
+            s.Method(2);
+            Console.ReadLine();
+        }
+        static void Main2(string[] args)
+        {
+            int i = 100;
+            i.Display();
+            MyExtensionMehtods.Display(i); // Internally it converted to this process
+            i.ExtMethod();
+
+            string s = "aaa";
+            s.Show();
+            MyExtensionMehtods.Show(s);
+
+            s.Method(2);
+
+            Console.ReadLine();
+        }
+        static void Main3(string[] args)
+        {
+            ClsMath obj = new ClsMath();
+            obj.ExtMethod();
+            Console.WriteLine(obj.Add(11, 22));
+            Console.ReadLine();
+        }
+        static int Reverse(int i)
+        {
+            int r = 0;
+            return r;
+        }
+    }
+    public static class MyExtensionMehtods
+    {
+        public static void ExtMethod(this object o)
+        {
+            Console.WriteLine(o);
+        }
+        public static void Display(this int i)
+        {
+            Console.WriteLine(i);
+        }
+        public static void Show(this string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void Method(this string s, int i)
+        {
+            Console.WriteLine(s+i);
+        }
+
+    }
+    public interface IMathOperation
+    {
+        int Add(int a, int b);
+    }
+    public class ClsMath : IMathOperation
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
+
+namespace LanguageFeatures5
+{
+    class Program
+    {
+        
+        static void Main3(string[] args)
+        {
+            Class1 o = new Class1();
+            
+            Console.ReadLine();
+        }
+       
+    }
+   public partial class Class1
+    {
+        public int i;
+    }
+    public partial class Class1
+    {
+        public int j;
+    }
+
+}
+
+namespace LanguageFeatures6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 o = new Class1();
+            Console.WriteLine(o.Check());
+            Console.ReadLine();
+        }
+
+    }
+    public partial class Class1
+    {
+        public int i;
+        private bool isValid = true;
+        partial void Validate();
+        public bool Check()
+        {
+            Validate();
+
+            return isValid;
+        }
+
+        
+    }
+    public partial class Class1
+    {
+        partial void Validate()
+        {
+            isValid = false;
+            Console.WriteLine("Validateion");
+        }
+    }
+
+}
